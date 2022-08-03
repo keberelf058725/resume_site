72c75d8c73c1e48364f885cf1e1ce5cc747a722e
keberelf058725 <kcaldonjr@gmail.com>
2022-08-03 19:11:31 -0400

    New commit

--- Resume/Resume/settings.py
+++ Resume/Resume/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ LOGIN_REDIRECT_URL = 'home'
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY='django-insecure-$s&uo1inu!1n3xi*g(=!&4p6)=na7hwt#=fiqf(qg@1&ab_eya'
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
