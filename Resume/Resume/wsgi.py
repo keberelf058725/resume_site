68fe530c5a56ca1508065b4f3158c77973af084e
keberelf058725 <kcaldonjr@gmail.com>
2022-08-06 16:14:53 -0400

    New commit

--- Resume/Resume/wsgi.py
+++ Resume/Resume/wsgi.py
@@ -1,16 +1,19 @@
 """
-WSGI config for Resume project.
+WSGI config for BH_DJANGO project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Resume.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BH_DJANGO.settings')
 
 application = get_wsgi_application()
+
+project_folder = os.path.expanduser('/home/kcaldonjr0587/resume_site/')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
