c20ec56cec84888cb4d95ee3e36d0b6dc0a8d283
keberelf058725 <kcaldonjr@gmail.com>
2022-08-06 22:29:07 -0400

    New commit

--- Resume/Resume/settings.py
+++ Resume/Resume/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kcaldonjr0587.pythonanywhere.com']
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
