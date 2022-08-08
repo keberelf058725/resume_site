eb051ee22ffe8310dcafba28ce84450b1c69e017
keberelf058725 <kcaldonjr@gmail.com>
2022-08-08 19:22:43 -0400

    New commit

---
+++ Resume/static/app.js
@@ -0,0 +1,5 @@
+gsap.fromTo(".H_grid", {opacity: 0}, {opacity: 1, duration: 2});
+gsap.fromTo(".H_grid", 1.2, {x: "-400"}, {x: '0%', ease: Power2.easeInOut});
+
+gsap.fromTo(".job_container", {opacity: 0}, {opacity: 1, duration: 2});
+gsap.fromTo(".job_container", 1.2, {y: "-400"}, {y: '0%', ease: Power2.easeInOut});
