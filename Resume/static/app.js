bd6b378a5096110bd34d1ecf9bd16b588c55d04a
keberelf058725 <kcaldonjr@gmail.com>
2022-08-08 20:43:23 -0400

    Add JavaScript

--- Resume/static/app.js
+++ Resume/static/app.js
@@ -1,7 +1,6 @@
 const tl = new TimelineMax();
 const tl2 = new TimelineMax();
 
-
 tl.fromTo("#H_grid1", 1, {opacity: 0}, {opacity: 1, duration: 2}
 ).fromTo("#H_grid1", 1, {x: "-400"}, {x: '0%', ease: Power2.easeInOut});
 
@@ -10,3 +9,6 @@ tl2.fromTo("#H_grid2", 1, {opacity: 0}, {opacity: 1, duration: 2}
 
 gsap.fromTo(".job_container", {opacity: 0}, {opacity: 1, duration: 2});
 gsap.fromTo(".job_container", 1.2, {y: "-400"}, {y: '0%', ease: Power2.easeInOut});
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.fromTo(".home_con_title", {opacity: 0}, {opacity: 1, duration: 3, scrollTrigger: ".home_con_title"});
